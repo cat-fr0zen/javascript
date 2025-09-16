e83b91fe7cefbf13c176c655b25afe2ea3749251
Ducret matthéo <mattheo.ducret@etu.unicaen.fr>
2025-09-16 09:26:39 +0200

    exercice 2 à 6

--- exercice_javascript/exercices.js
+++ exercice_javascript/exercices.js
@@ -1,5 +1,7 @@
 console.log("Hello World !")
 
+
+// exercice 1
 const degres_celsius = -17;
 
 function celsius_to_fahrenheit(celsius){
@@ -7,3 +9,47 @@ function celsius_to_fahrenheit(celsius){
 }
 
 console.log(celsius_to_fahrenheit(degres_celsius))
+
+// exercice 2
+
+const longeur = 2;
+const Largeur = 3;
+
+function calcul_aire_rectangle(L,l){
+    return L * l;
+}
+
+console.log(calcul_aire_rectangle(Largeur,longeur))
+
+//exercice 3
+
+const prenom = "mille";
+const nom = "pertuis";
+
+console.log("vous êtes " + prenom + " " + nom)
+
+//exercice 4 
+
+const montant_ht = 20.99;
+const TVA = 1.055
+
+function montant_ttc(montant,tva){
+    return montant * TVA
+}
+
+console.log(montant_ttc(montant_ht,TVA))
+
+//exercice 5
+
+console.log(((4 >= 6) || ("herbe" != "verte")) && !(((12 * 2) == 144) && true))
+
+//exercice 6 
+
+const poids = 65;
+const taille = 1.63;
+
+function CalculIMC(p,T){
+    const imc = p / taille * taille ;
+    return tofixed(imc);
+}
+console.log(CalculIMC(poids,taille))
