efcd85d0b12b2c7c3956a4ef59b47b68ce64211a
Ducret matthéo <mattheo.ducret@etu.unicaen.fr>
2025-09-16 08:54:03 +0200

    exercice_1_base

--- exercice_javascript/exercices.js
+++ exercice_javascript/exercices.js
@@ -1,1 +1,9 @@
 console.log("Hello World !")
+
+const degres_celsius = -17;
+
+function celsius_to_fahrenheit(celsius){
+    return celsius * 9/5 +32;
+}
+
+console.log(celsius_to_fahrenheit(degres_celsius))
